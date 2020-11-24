af0cdf0341e13fb3512daed03585338c8704010f
wuxinle@unipus.cn <wuxinle@unipus.cn>
2020-11-24 17:11:53 +0800

    commit message: 线程交替打印。

---
+++ src/main/java/cn/unipus/java/learning/concurrency/exercise/PrintOrderByLock.java
@@ -0,0 +1,13 @@
+package cn.unipus.java.learning.concurrency.exercise;
+
+/**
+ * @author wuxinle
+ * @version 1.0
+ * @date 2020/11/24 17:10
+ * Description: 使用ReentrantLock实现线程间交替打印
+ */
+public class PrintOrderByLock {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/cn/unipus/java/learning/concurrency/exercise/PrintOrderByWaitNotify.java
@@ -0,0 +1,10 @@
+package cn.unipus.java.learning.concurrency.exercise;
+
+/**
+ * @author wuxinle
+ * @version 1.0
+ * @date 2020/11/24 17:09
+ * Description: 使用wait notify实现线程之间交替打印
+ */
+public class PrintOrderByWaitNotify {
+}
